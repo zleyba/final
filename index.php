72a0eba2d955f53f445a177b963050e40d06cee7
zleyba <mtvzach@gmail.com>
2016-03-08 01:12:47 -0700

    cleaned up head code

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<?php require_once(dirname(__DIR__) . "/public_html/lib/head-tags.html"); ?>
+		<?php require_once(dirname(__DIR__) . "final/lib/head-tags.html"); ?>
 		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Didot' rel='stylesheet' type='text/css'>
 
 		<!-- loads gallery support files-->
-		<link rel="stylesheet" href="../public_html/css/swipebox.css">
-		<script src="../public_html/lib/jquery-2.1.0.min.js"></script>
-		<script src="../public_html/js/jquery.swipebox.js"></script>
+		<link rel="stylesheet" href="/css/swipebox.css">
+		<script src="lib/jquery-2.1.0.min.js"></script>
+		<script src="js/jquery.swipebox.js"></script>
 
 		<script type="text/javascript">
 			;( function( $ ) {
@@ -29,13 +29,13 @@
 		<!--Nav bar Scripts-->
 
 		<!--Navbar Transformation script-->
-		<script type="text/javascript" src="../public_html/js/navtransform.js"></script>
+		<script type="text/javascript" src="js/navtransform.js"></script>
 
 
 
 
 		<!--main style sheet-->
-		<link rel="stylesheet" href="../public_html/css/final.css" />
+		<link rel="stylesheet" href="css/final.css" />
 
 		<title>The Personal Website of Illustrator Kelly Williams</title>
 
